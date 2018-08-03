c0b1e64647aabf271f6cc92eb49b52ac3f7fb43f
saulo <saulo.firmino@brilliantmachine.com.br>
2018-08-03 12:59:13 -0300

    término do curso

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,7 +1,9 @@
 const BillingCycle = require('./billingCycle')
+const errorHandler = require('../commom/errorHandler')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
+BillingCycle.after('post',errorHandler).after('put',errorHandler)
 
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((error,value) => {
